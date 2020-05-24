b8b6c9b3ccb6e66b943dd55e41e15620b8e8d355
李志钊 <lizhizhaooo@163.com>
2020-05-24 11:49:13 +0800

    替换一下库

---
+++ src/test/java/com/lizhizhao/springboot/blog/LBlog/LBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizhizhao.springboot.blog.LBlog;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
